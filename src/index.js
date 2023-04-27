c7a9eed9c98eaeb11a580471b7fff6438e1c4a58
Beck LaBash <55890162+becklabs@users.noreply.github.com>
2023-04-27 19:13:03 -0400

    Analytics

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
+import { inject } from '@vercel/analytics';
+inject();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
